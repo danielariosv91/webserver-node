935d1021263fd2d09097682d45de50661863aff8
Daniela Rios <daniela.rios.v91@gmail.com>
2023-09-05 23:50:16 -0300

    create partials

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import hbs from 'hbs';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 
@@ -8,25 +9,31 @@ const __dirname = dirname(__filename);
 const app = express();
 const port = 8080;
 
-/** Handlebars */
-app.set('view engine', 'hbs')
+
+/** Handlebars and partials */
+app.set('view engine', 'hbs'); 
+hbs.registerPartials(__dirname + '/views/partials');
 
 /** Public middleware:: static content */
 app.use(express.static('public'));
 
 app.get('/', (request, response) => {
-    response.render('home', { 
-        title: 'Webserver Node App', 
-        name: 'danielariosv' 
+    response.render('home', {
+        title: 'Webserver Node App',
+        name: 'danielariosv'
     });
 });
 
 app.get('/generic', (request, response) => {
-    response.sendFile(__dirname + '/public/generic.html');
+    response.render('generic', {
+        title: 'Generic - Road Trip by TEMPLATED',
+    });
 });
 
 app.get('/elements', (request, response) => {
-    response.sendFile(__dirname + '/public/elements.html');
+    response.render('generic', {
+        title: 'Elements - Road Trip by TEMPLATED',
+    });
 });
 
 app.get('*', (request, response) => {
